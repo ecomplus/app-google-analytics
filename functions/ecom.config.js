49677f4873b36fdbb2de9efdba190f6c95156825
Wisley Alves <wisley7lagoas@yahoo.com.br>
2022-12-30 15:26:07 -0300

    chore(app): init

--- functions/ecom.config.js
+++ functions/ecom.config.js
@@ -82,14 +82,14 @@ const app = {
       // 'DELETE',        // Delete customers
     ],
     orders: [
-      // 'GET',           // List/read orders with public and private fields
+      'GET',           // List/read orders with public and private fields
       // 'POST',          // Create orders
       // 'PATCH',         // Edit orders
       // 'PUT',           // Overwrite orders
       // 'DELETE',        // Delete orders
     ],
     carts: [
-      // 'GET',           // List all carts (no auth needed to read specific cart only)
+      'GET',           // List all carts (no auth needed to read specific cart only)
       // 'POST',          // Create carts
       // 'PATCH',         // Edit carts
       // 'PUT',           // Overwrite carts
@@ -138,37 +138,33 @@ const app = {
   },
 
   admin_settings: {
-    /**
-     * JSON schema based fields to be configured by merchant and saved to app `data` / `hidden_data`, such as:
-
-     webhook_uri: {
-       schema: {
-         type: 'string',
-         maxLength: 255,
-         format: 'uri',
-         title: 'Notifications URI',
-         description: 'Unique notifications URI available on your Custom App dashboard'
-       },
-       hide: true
-     },
-     token: {
-       schema: {
-         type: 'string',
-         maxLength: 50,
-         title: 'App token'
-       },
-       hide: true
-     },
-     opt_in: {
-       schema: {
-         type: 'boolean',
-         default: false,
-         title: 'Some config option'
-       },
-       hide: false
-     },
-
-     */
+    firebase_app_id: {
+      schema: {
+        type: 'string',
+        maxLength: 500,
+        title: 'API Secret',
+        description: 'ID do app do Firebase [Consultar Documentação](https://developers.google.com/analytics/devguides/collection/protocol/ga4/sending-events?client_type=firebase#required_parameters)'
+      },
+      hide: true
+    },
+    api_secret: {
+      schema: {
+        type: 'string',
+        maxLength: 500,
+        title: 'API Secret',
+        description: 'Uma API Secret gerada na IU do Google Analytics. [Consultar Documentação](https://developers.google.com/analytics/devguides/collection/protocol/ga4/sending-events?client_type=firebase#required_parameters)'
+      },
+      hide: true
+    },
+    // client_id: {
+    //   schema: {
+    //     type: 'string',
+    //     maxLength: 100,
+    //     title: 'Pixel ID',
+    //     description: ''
+    //   },
+    //   hide: true
+    // },
   }
 }
 
@@ -181,7 +177,7 @@ const procedures = []
 
 /**
  * Uncomment and edit code above to configure `triggers` and receive respective `webhooks`:
-
+*/
 const { baseUri } = require('./__env')
 
 procedures.push({
@@ -193,7 +189,7 @@ procedures.push({
       resource: 'orders',
       action: 'create',
     },
-
+    /*
     // Receive notifications when order financial/fulfillment status are set or changed:
     // Obs.: you probably SHOULD NOT enable the orders triggers below and the one above (create) together.
     {
@@ -229,6 +225,7 @@ procedures.push({
     },
 
     // Feel free to create custom combinations with any Store API resource, subresource, action and field.
+    // */
   ],
 
   webhooks: [
@@ -242,7 +239,7 @@ procedures.push({
     }
   ]
 })
-
+/*
  * You may also edit `routes/ecom/webhook.js` to treat notifications properly.
  */
 
--- functions/routes/ecom/webhook.js
+++ functions/routes/ecom/webhook.js
@@ -1,12 +1,42 @@
 // read configured E-Com Plus app data
+const { default: axios } = require('axios')
 const getAppData = require('./../../lib/store-api/get-app-data')
+
+const Axios = require(axios)
+
+const createAxios = () => {
+  const headers = {
+    'Content- Type': application / json
+  }
+
+  return axios.create({
+    baseURL: 'www.google-analytics.com',
+    headers
+  })
+}
 
 const SKIP_TRIGGER_NAME = 'SkipTrigger'
 const ECHO_SUCCESS = 'SUCCESS'
 const ECHO_SKIP = 'SKIP'
 const ECHO_API_ERROR = 'STORE_API_ERR'
 
-exports.post = ({ appSdk }, req, res) => {
+// const getAuth = (appSdk, storeId) => new Promise((resolve, reject) => {
+//   appSdk.getAuth(storeId)
+//     .then(auth => resolve(auth))
+//     .catch(err => reject(err))
+// })
+
+// const findOrderById = (appSdk, storeId, orderId, auth) => new Promise((resolve, reject) => {
+//   appSdk.apiRequest(storeId, `/orders/${orderId}.json`, 'GET', null, auth)
+//     .then(({ response }) => {
+//       resolve(response.data)
+//     })
+//     .catch(err => {
+//       reject(err)
+//     })
+// })
+
+exports.post = ({ appSdk, admin }, req, res) => {
   // receiving notification from Store API
   const { storeId } = req
 
@@ -16,46 +46,78 @@ exports.post = ({ appSdk }, req, res) => {
    */
   const trigger = req.body
 
-  // get app configured options
-  getAppData({ appSdk, storeId })
+  if (trigger.resource === 'orders' && trigger.action === 'create') {
+    const orderId = trigger.inserted_id
+    let order = trigger.body
+    if (order.status === 'cancelled') {
+      res.sendStatus(204)
+      return
+    }
+    const buyer = order.buyers && order.buyers[0]
+    const clientIp = order.browser_ip
+    if (orderId && buyer && clientIp) {
 
-    .then(appData => {
-      if (
-        Array.isArray(appData.ignore_triggers) &&
-        appData.ignore_triggers.indexOf(trigger.resource) > -1
-      ) {
-        // ignore current trigger
-        const err = new Error()
-        err.name = SKIP_TRIGGER_NAME
-        throw err
-      }
+      // get app configured options
+      getAppData({ appSdk, storeId })
 
-      /* DO YOUR CUSTOM STUFF HERE */
+        .then(appData => {
+          if (
+            Array.isArray(appData.ignore_triggers) &&
+            appData.ignore_triggers.indexOf(trigger.resource) > -1
+          ) {
+            // ignore current trigger
+            const err = new Error()
+            err.name = SKIP_TRIGGER_NAME
+            throw err
+          }
 
-      // all done
-      res.send(ECHO_SUCCESS)
-    })
+          const firebaseAppId = appData.firebase_app_id
+          const apiSecret = appData.api_secret
+          if (firebaseAppId && apiSecret) {
+            // url = `/mp/collect?firebase_app_id=${firebaseAppId}&api_secret=${apiSecret}`
+            // const axios = createAxios()
+            // const intanceId = admin.instanceId()
+            // const body = {
+            //   app_instance_id: intanceId,
+            //   events: [{
+            //     name: 'tutorial_begin',
+            //     params: {},
+            //   }]
+            // }
+            // console.log('>>intanceId: ', intanceId)
 
-    .catch(err => {
-      if (err.name === SKIP_TRIGGER_NAME) {
-        // trigger ignored by app configuration
-        res.send(ECHO_SKIP)
-      } else if (err.appWithoutAuth === true) {
-        const msg = `Webhook for ${storeId} unhandled with no authentication found`
-        const error = new Error(msg)
-        error.trigger = JSON.stringify(trigger)
-        console.error(error)
-        res.status(412).send(msg)
-      } else {
-        // console.error(err)
-        // request to Store API with error response
-        // return error status code
-        res.status(500)
-        const { message } = err
-        res.send({
-          error: ECHO_API_ERROR,
-          message
+          } else {
+            console.log('>> firebaseAppId or apiSecret not found')
+            res.status(400).send(ECHO_SKIP)
+            return
+          }
+
+          // res.send(ECHO_SUCCESS)
         })
-      }
-    })
+
+        .catch(err => {
+          if (err.name === SKIP_TRIGGER_NAME) {
+            // trigger ignored by app configuration
+            res.send(ECHO_SKIP)
+          } else if (err.appWithoutAuth === true) {
+            const msg = `Webhook for ${storeId} unhandled with no authentication found`
+            const error = new Error(msg)
+            error.trigger = JSON.stringify(trigger)
+            console.error(error)
+            res.status(412).send(msg)
+          } else {
+            // console.error(err)
+            // request to Store API with error response
+            // return error status code
+            res.status(500)
+            const { message } = err
+            res.send({
+              error: ECHO_API_ERROR,
+              message
+            })
+          }
+        })
+    }
+  }
+  res.sendStatus(412)
 }
